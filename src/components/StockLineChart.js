774860198829acedc2ca7d59a0126f4f308115b3
kanakdeepa <kanakdeepa.pradhan91@gmail.com>
2019-09-29 00:27:50 +0800

    Bonus task Completed

---
+++ src/components/StockLineChart.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+
+const StockLineChart = props => {
+  const [lineData, setLineData] = useState(null);
+
+  const colors = [
+    'red',
+    'green',
+    'blue',
+    'black',
+    'teal',
+    '#d64161',
+    '#80ced6',
+    '#d4ac6e',
+    '#667292',
+  ];
+  const usedColors = [];
+
+  const generateRandomColor = () => {
+    let newColor = colors[Math.round(Math.random() * (colors.length - 1))];
+    while (usedColors.some(c => c === newColor)) {
+      newColor = colors[Math.round(Math.random() * (colors.length - 1))];
+    }
+    usedColors.push(newColor);
+    return newColor;
+  };
+
+  useEffect(() => {
+    const { data: allData } = props;
+    const params = props.params || ['open', 'close'];
+
+    setLineData({
+      labels: allData.map(record => record.date),
+      datasets: params.map(param => ({
+        label: param.charAt(0).toUpperCase() + param.substring(1),
+        borderColor: generateRandomColor(),
+        backgroundColor: 'transparent',
+        borderWidth: 2,
+        data: allData.map(record => record[param]),
+      })),
+    });
+  }, [props.data, props.params]);
+
+  return lineData ? <Line data={lineData} /> : null;
+};
+
+export default StockLineChart;
--- src/pages/StockDetails.js
+++ src/pages/StockDetails.js
@@ -4,6 +4,7 @@ import { Table, Row, Col, Button, Card, CardBody, CardText } from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { showCompanyDetails, showChart } from '../actions';
 import { withRouter } from 'react-router';
+import StockLineChart from '../components/StockLineChart';
 
 // import { Line } from 'react-chartjs-2';
 
@@ -51,6 +52,14 @@ const StockDetails = props => {
   const companyDetails = useSelector(state => state.stockState.companyDetails);
   const chartData = useSelector(state => state.stockState.chart);
   const [duration, setDuration] = useState('1m');
+  const [chartOptions, setChartOptions] = useState({
+    open: true,
+    close: true,
+    high: false,
+    low: false,
+    volume: false,
+    change: false,
+  });
 
   const dispatch = useDispatch();
 
@@ -68,48 +77,78 @@ const StockDetails = props => {
     setDuration(duration);
   };
 
+  const allStyles = {
+    spanStyle: {
+      color: 'grey',
+      fontSize: 12,
+      fontWeight: 600,
+      margin: '0px 8px',
+      textTransform: 'capitalize',
+    },
+  };
+  const checkboxHandler = opt => {
+    const chrtOpts = { ...chartOptions };
+    chrtOpts[opt] = !chrtOpts[opt];
+    setChartOptions(chrtOpts);
+  };
   return (
     <Page title={companyDetails && companyDetails.companyName}>
       <Card style={{ marginBottom: 15 }}>
         <CardBody>{companyDetails && companyDetails.description}</CardBody>
       </Card>
 
-      {/* <Line data={genLineData({ fill: false }, { fill: false })} /> */}
-      <Card>
-        {chartData && chartData === 'NOT_FOUND' ? (
-          <div
-            style={{
-              background: 'orange',
-              padding: '10px 15px',
-              borderRadius: 3,
-            }}
-          >
-            Data Not Found
+      {chartData && chartData === 'NOT_FOUND' ? (
+        <div
+          style={{
+            background: 'orange',
+            padding: '10px 15px',
+            borderRadius: 3,
+          }}
+        >
+          Data Not Found
+        </div>
+      ) : chartData.length ? (
+        <>
+          <Row>
+            <Col xs={12} sm={6}>
+              <Button
+                outline={!(duration === '1m')}
+                onClick={() => durationChangeHandler('1m')}
+              >
+                1 Month
+              </Button>{' '}
+              <Button
+                outline={!(duration === '3m')}
+                onClick={() => durationChangeHandler('3m')}
+              >
+                3 Month
+              </Button>{' '}
+              <Button
+                outline={!(duration === '6m')}
+                onClick={() => durationChangeHandler('6m')}
+              >
+                6 Month
+              </Button>
+            </Col>
+          </Row>
+
+          <div style={{ float: 'right' }}>
+            {Object.keys(chartOptions).map((opt, i) => (
+              <span key={i}>
+                <input
+                  type="checkbox"
+                  checked={chartOptions[opt]}
+                  onChange={() => checkboxHandler(opt)}
+                />
+                <span style={allStyles.spanStyle}>{opt}</span>
+              </span>
+            ))}
           </div>
-        ) : chartData.length ? (
-          <>
-            <Row>
-              <Col xs={12} sm={6}>
-                <Button
-                  outline={!(duration === '1m')}
-                  onClick={() => durationChangeHandler('1m')}
-                >
-                  1 Month
-                </Button>{' '}
-                <Button
-                  outline={!(duration === '3m')}
-                  onClick={() => durationChangeHandler('3m')}
-                >
-                  3 Month
-                </Button>{' '}
-                <Button
-                  outline={!(duration === '6m')}
-                  onClick={() => durationChangeHandler('6m')}
-                >
-                  6 Month
-                </Button>
-              </Col>
-            </Row>
+          <StockLineChart
+            data={chartData}
+            params={Object.keys(chartOptions).filter(opt => chartOptions[opt])}
+          />
+          <Card>
             <Table size="sm">
               <thead>
                 <tr>
@@ -136,9 +175,9 @@ const StockDetails = props => {
                 ))}
               </tbody>
             </Table>
-          </>
-        ) : null}
-      </Card>
+          </Card>
+        </>
+      ) : null}
     </Page>
   );
 };
